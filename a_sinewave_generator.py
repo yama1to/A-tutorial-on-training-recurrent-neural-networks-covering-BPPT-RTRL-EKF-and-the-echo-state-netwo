577d88570f7ab94ede015d43f8a63d42fec8eb1c
yamato <yamatosakino1113@icloud.com>
2021-02-20 11:21:04 +0900

    200/300個はそこそこいい精度

--- a_sinewave_generator.py
+++ a_sinewave_generator.py
@@ -1,31 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-"""
-input 0  
-DR 20 ⇨x
-out 1 ⇨y
-
-W_out x→y
-W_back y→x
-W x→x
-
-M
-T
-"""
-
 class sinewave_generator:
 	def __init__(self,x_nodes_num,y_nodes_num,learning_times,f = np.tanh):
 
 		#reservoir層の重みを記録
-		#self.x = np.array([np.zeros(x_nodes_num)])
-		self.x = self.standard_init_weight(y_nodes_num,x_nodes_num).astype('float64')
-		
+		self.x = self.standard_init_weight(y_nodes_num,x_nodes_num)
+
 		#重み 
-		self.w = self.standard_init_weight(x_nodes_num,x_nodes_num).astype('float64')
-		self.w_back = self.standard_init_weight(y_nodes_num,x_nodes_num).astype('float64')
+		self.w = self.standard_init_weight(x_nodes_num,x_nodes_num)
+		self.w_back = self.standard_init_weight(y_nodes_num,x_nodes_num)
 		#更新されるのはw_outだけ。
-		self.w_out = self.standard_init_weight(x_nodes_num,y_nodes_num).astype('float64')
+		self.w_out = self.standard_init_weight(x_nodes_num,y_nodes_num)
 
 
 
@@ -40,23 +26,21 @@ class sinewave_generator:
 
 	#出力層の重みの更新
 	def update_w_out(self):
-		M = self.x[101:].astype('float64')
+		M = self.x[101:]
 		#print(M.shape)
-		T = np.array([[self.d(n)] for n in range(101,len(self.x))]).astype('float64')
+		T = np.array([[self.d(n)] for n in range(101,len(self.x))])
 		w_out = np.linalg.pinv(M) @ T
-		self.w_out = w_out.astype('float64')
+		self.w_out = w_out
 
 	#reservoir層の次状態を取得
 	def next_x(self,y):
 		
 		a = self.w @ np.array([self.x[-1]]).T
 		b = self.w_back.T @ y
-		x_next_state = self.f(a+b).T.astype('float64')
+		x_next_state = self.f(a+b).T
 		
-
 		#x_next_state = self.f(self.w_back.T @ y).T
-
-		return x_next_state * 0.25 #(1,20)
+		return x_next_state *0.2 #(1,20)
 
 	#訓練
 	def train(self,learning_times):
@@ -66,7 +50,7 @@ class sinewave_generator:
 			#y = np.sum(self.w_out @ np.array([self.x[-1]]))
 			y = self.d(i)
 			#print(y)
-			y = np.array([[y]]).astype('float64')
+			y = np.array([[y]])
 			#print(self.x.shape)
 			self.x = np.append(self.x,self.next_x(y),axis = 0)
 			#print(self.x.shape,self.next_x(y).shape)
@@ -92,13 +76,13 @@ class sinewave_generator:
 	def MSE(self,start,end,predicted_outputs):
 		mse = 0
 		for n in range(start,end+1):
-			diff = self.d(n-1) - predicted_outputs[n-start].astype('float64')
+			diff = self.d(n-1) - predicted_outputs[n-start]
 			#print(diff)
 			#print(diff)
 			mse += (diff)**2
 			#print(n-start)
 		mse = mse/200
-		print("教師と予測の平均２乗誤差は ["+str(mse)+"]")#目標は 5.6e-12の誤差
+		return mse
 
 	#教師信号
 	def d(self,n):
@@ -106,7 +90,7 @@ class sinewave_generator:
 
 	#初期値
 	def standard_init_weight(self,c,v):
-		w = np.random.normal(0,1,c*v).astype('float64').reshape([c,v])
+		w = np.random.normal(0,1,c*v).reshape([c,v])
 		return w
 
 	#初期値
@@ -130,6 +114,15 @@ class sinewave_generator:
 		plt.show()
 		#print(self.x.shape)
 
+	def p_plot(self,learning_times,predict_times,predict):
+		x_test = list(range(learning_times,learning_times+predict_times+1))
+		x_train = range(1,learning_times)
+		y_train = list(self.d(n) for n in range(1,learning_times))
+
+		plt.plot(x_train,y_train)
+		plt.plot(x_test,predict)
+		plt.show()
+
 def main():
 
 	x_nodes_num = 20
@@ -144,30 +137,32 @@ def main():
 		y_nodes_num = y_nodes_num,
 		learning_times = learning_times)
 
-	model.train(learning_times)
+	model.train(learning_times)#train
+	predict = model.predict(learning_times,predict_times)#predict
+	mse = model.MSE(learning_times,learning_times+predict_times,predict)
+	print("教師と予測の平均２乗誤差は ["+str(mse)+"]")#目標は 5.6e-12の誤差
 
 	
-	#print(predict)
-	x_test = list(range(learning_times,learning_times+predict_times+1))
-	x_train = range(1,learning_times)
-	y_train = list(model.d(n) for n in range(1,learning_times))
+	#model.p_plot(learning_times,predict_times,predict)
+
+	#r = list(range(101,151))
+	#model.x_plot(r)
+
+	return mse
 
 
-	predict = model.predict(learning_times,predict_times)
-	model.MSE(learning_times,learning_times+predict_times,predict)
-	
-	plt.plot(x_train,y_train)
-	plt.plot(x_test,predict)
-	plt.show()
-	
-	r = list(range(101,151))
-	#print(r)
-	model.x_plot(r)
-
-	#print(np.array([np.zeros(x_nodes_num)]))
-	#print(np.array([np.random.normal(0,1,x_nodes_num)]))
 
 main()
+
+
+mse = []
+for i in range(300):
+	if main() < 5.6e-8:
+		mse.append(main())
+
+mse = sorted(mse)
+print(len(mse))
+
 
 
 
